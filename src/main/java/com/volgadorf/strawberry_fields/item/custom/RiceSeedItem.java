5d34cda94346413a62f75f674025628ed31a53c4
Joey Trauger <jjtrauger2@gmail.com>
2023-05-06 18:39:59 -1000

    Rice Seed Composter Tweak
    using rice seeds on composter now acts like other seeds do, but there is a bug. if full, composter does not auto transition to give bonemeal state. also sometimes it goes back down when used. requires playtesting

--- src/main/java/com/volgadorf/strawberry_fields/item/custom/RiceSeedItem.java
+++ src/main/java/com/volgadorf/strawberry_fields/item/custom/RiceSeedItem.java
@@ -1,6 +1,8 @@
 package com.volgadorf.strawberry_fields.item.custom;
 
 import com.volgadorf.strawberry_fields.item.ModFoodItems;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
@@ -15,6 +17,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
+import java.util.Random;
 
 public class RiceSeedItem extends PlaceOnWaterBlockItem {
     public RiceSeedItem(Block p_220226_, Properties p_220227_) {
@@ -32,20 +35,39 @@ public class RiceSeedItem extends PlaceOnWaterBlockItem {
         Block block = blockState.getBlock();
         Player player = context.getPlayer();
         if (block instanceof ComposterBlock) {
+            Random random = new Random();
             int currentValue = blockState.getValue(ComposterBlock.LEVEL);
             if (currentValue < 7) { // check if current value is less than 7
-                BlockState newState = blockState
-                        .setValue(ComposterBlock.LEVEL, currentValue + 1); // set the new value
-                context.getLevel().setBlock(context.getClickedPos(), newState, 3); // update the block state
+                if (random.nextDouble() < 0.5) { //only fill it up 50% of time to simulate behavior similar to wheat seeds
+                    BlockState newState = blockState
+                            .setValue(ComposterBlock.LEVEL, currentValue + 1); // set the new value
+                    context.getLevel().setBlock(context.getClickedPos(), newState, 3); // update the block state
+                }
+
                 context.getLevel().playSound(null, context.getClickedPos(), SoundEvents.COMPOSTER_FILL, SoundSource.BLOCKS, 1.0f, 1.0f);
                 assert player != null;
                 if (player.getMainHandItem().getItem().equals(ModFoodItems.RICE_SEED.get())){
                     Objects.requireNonNull(context.getPlayer()).swing(InteractionHand.MAIN_HAND);
+                    player.getMainHandItem().shrink(1);
                 }
                 else{
                     Objects.requireNonNull(context.getPlayer()).swing(InteractionHand.OFF_HAND);
+                    player.getOffhandItem().shrink(1);
                 }
 
+                // Play the green particle effect
+                double x = context.getClickedPos().getX() + 0.5;
+                double y = context.getClickedPos().getY() + 0.5;
+                double z = context.getClickedPos().getZ() + 0.5;
+                if (context.getLevel() instanceof ServerLevel) {
+                    ((ServerLevel) context.getLevel()).sendParticles(ParticleTypes.COMPOSTER, x, y, z, 10, 0.2, 0.2, 0.2, 0.0);
+                }
+            } else{
+                // Composter is full, transition to the bonemeal state
+                BlockState newState = blockState.setValue(ComposterBlock.LEVEL, 0)
+                                .setValue(ComposterBlock.LEVEL, ComposterBlock.READY);
+                context.getLevel().setBlock(context.getClickedPos(), newState, 3);
+                context.getLevel().levelEvent(1500, context.getClickedPos(), 0);
             }
         } else {
             System.out.println("what the");
